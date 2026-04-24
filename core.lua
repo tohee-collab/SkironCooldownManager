62e675c14633749e66646e9cd07cd75520f61b27
Permok <martinw1394@gmail.com>
2026-04-24 22:47:20 +0200

    Sometimes Blizzard rebuilds the full cooldown manager in combat so reduce the performance a little bit by always checking all essential und utilit cooldown viewer children when one of them gets updated.

--- core.lua
+++ core.lua
@@ -18,11 +18,13 @@ SCM.Templates = {}
 
 local function OnEssentialCooldownViewerLayout(viewer)
 	SCM:InvalidateViewerChildrenCache(viewer)
+	SCM:InvalidateViewerChildrenCache(UtilityCooldownViewer)
 	SCM:ApplyEssentialCDManagerConfig()
 end
 
 local function OnUtilityCooldownViewerLayout(viewer)
 	SCM:InvalidateViewerChildrenCache(viewer)
+	SCM:InvalidateViewerChildrenCache(EssentialCooldownViewer)
 	SCM:ApplyUtilityCDManagerConfig()
 end
 
